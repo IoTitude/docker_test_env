0756b9fd439f07322a710b28658fc549c8edfc27
Jaakko Pöyhönen <k1803@student.jamk.fi>
2016-06-15 10:19:57 +0300

    made files

---
+++ firefox_env/scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+date
+# firefox -version
+firefox --version 2>/dev/null
+echo 'Starting Xvfb ...'
+export DISPLAY=:99
+2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
+exec "$@"
+echo 'Cloning test repo...'
+git clone https://github.com/IoTitude/Tests.git  /home/root/test
+echo 'Starting robot tests...'
+robot /home/root/test/tests.txt
+mv /log.html /home/root/volume
+mv /output.xml /home/root/volume
+mv /report.html /home/root/volume
