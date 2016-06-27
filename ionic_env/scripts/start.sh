78ebbfc202ecca2cecc34dfd4a7992da60f5b353
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-06-27 11:57:58 +0300

    Update start.sh

--- ionic_env/scripts/start.sh
+++ ionic_env/scripts/start.sh
@@ -5,5 +5,6 @@ cd /usr/src/Instapp
 git fetch --tags
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latestTag
+sed -i "/BASE_URL/c\  'BASE_URL': 'http://localhost:9100',/" /usr/src/Instapp/www/js/services.js
 echo 'Starting Instapp' $latestTag
 sh /usr/src/scripts/instapp.sh
