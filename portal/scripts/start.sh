60f40954afb2e63ff8e6581a3a37df24f32a74e1
Jaakko Pöyhönen <k1803@student.jamk.fi>
2016-07-18 11:01:56 +0300

    portal container

---
+++ portal/scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'Cloning Portal repo...'
+git clone https://github.com/IoTitude/Portal.git  /usr/src/Portal
+cd /usr/src/Portal
+git fetch --tags
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+git checkout $latestTag
+sed -i "/baseUrl/c\  baseUrl = 'http://localhost:9000'/" /usr/src/Portal/app/services/baasbox.service.ts
+echo 'Starting Portal' $latestTag
+sh /usr/src/scripts/portal.sh
