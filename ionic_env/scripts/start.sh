e4d9ccf63ac3ed9ef60e1669f60050dba41837fd
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-20 12:14:41 +0300

    Update start.sh

--- ionic_env/scripts/start.sh
+++ ionic_env/scripts/start.sh
@@ -5,8 +5,8 @@ cd /usr/src/Instapp
 git fetch --tags
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latestTag
-npm install
-ionic resources
-sed -i "/BASE_URL/c\  'BASE_URL': 'http://localhost:9000',/" /usr/src/Instapp/www/js/baasbox/baasbox.config.js
+source setup.sh
+sed -i "/BASE_URL/c\ 'BASE_URL': 'http://localhost:9000'," /usr/src/Instapp/www/js/baasbox/baasbox.config.js
+gulp minjs
 echo 'Starting Instapp' $latestTag
-ionic serve --all --nobrowser --nolivereload --port=8100
+ionic serve --all --nobrowser --nolivereload --port=8100 
