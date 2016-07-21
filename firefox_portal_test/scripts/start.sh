cf14c296d8b23b7ca2989f7907bcc92bcf81a429
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-21 14:22:27 +0300

    Update start.sh

--- firefox_portal_test/scripts/start.sh
+++ firefox_portal_test/scripts/start.sh
@@ -18,6 +18,17 @@ git pull origin master
 echo 'Starting robot tests...'
 cd /home/root/test/robot-framework
 testList=$(awk '/firefox/,/@@@/{if ($0!="@@@" && $2!="firefox"){print}}' tests.txt)
+# test connection loop
+success=""
+length=${#success}
+while test $length -eq 0
+do
+echo "Waiting for Portal..."
+success=$(curl -s --head http://localhost:3000/ | head -n 1 | grep "HTTP/1.[01] [23]..")
+length=${#success}
+echo $success
+sleep 5
+done
 robot $testList
 echo 'Copying test results...'
 mv /home/root/test/robot-framework/log.html /var/jenkins_home/volume/log.html
