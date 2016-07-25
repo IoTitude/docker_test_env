a01d8b6ce79eac8ba7660414056bf2078b185b2c
Jaakko Pöyhönen <k1803@student.jamk.fi>
2016-07-25 13:55:08 +0300

    removed files directory

--- baasbox_test/scripts/start.sh
+++ baasbox_test/scripts/start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-mkdir -p /usr/src/baasbox/db/baasbox
-echo "Copying plugins..."
-
-mv /usr/src/baasbox/files/_bb_script.cpm /usr/src/baasbox/db/baasbox/_bb_script.cpm
-mv /usr/src/baasbox/files/_bb_script.pcl /usr/src/baasbox/db/baasbox/_bb_script.pcl
-mv /usr/src/baasbox/files/_BB_Script.name.sbt /usr/src/baasbox/db/baasbox/_BB_Script.name.sbt
-mv /usr/src/baasbox/files/baasbox.0.wal /usr/src/baasbox/db/baasbox/baasbox.0.wal
-
 /usr/src/baasbox/scripts/data.sh & /usr/src/baasbox/start
-
-
